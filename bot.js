45e6e812e6dec0401762c1dbfdb53ff787a617a5
Lidel Kim Daddie <murdoc21daddie@gmail.com>
2021-06-10 06:47:46 +0800

    Fixed color and size select bugs

--- bot.js
+++ bot.js
@@ -12,27 +12,8 @@ const supreme = require("./shop/supreme");
 // const preferredCategoryName = "skate";
 // const preferredTitle = "Supreme®/Spitfire® Classic Wheels(Set of 4)";
 
-const userBot = {
-    bot1 : {        
-        preferredCategoryName : "jackets",
-        preferredTitle : "Eagle Hooded Work Jacket",
-        preferredColor : "Black", 
-        preferredSize : "XLarge", 
-        preferredQuantity : "4", 
-        preferredBillingName : "Test One", 
-        preferredOrder_email : "testone@testone.com", 
-        preferredOrder_number : "123 456 789 0", 
-        preferredOrder_billing_address : "2181 PO BOX", 
-        preferredOrder_billing_city : "ALABASTER", 
-        preferredOrder_billing_zip : "35007-2024", 
-        preferredOrder_billing_state : "AL", 
-        preferredCreditCardNumber : "12312321321312321312", 
-        preferredCcnMonth : "10", 
-        preferredCcnYear : "2031", 
-        preferredCcnCVV : "123",
-        preferredProxyServer : '216.21.18.193:80'
-    },
-    bot2 : {
+const userBotData = {
+    bot1 : {
         preferredCategoryName : "accessories",
         preferredTitle : "Supreme®/Hanes® Tagless Tank Tops (3 Pack)",
         preferredColor : "Black", 
@@ -50,11 +31,48 @@ const userBot = {
         preferredCcnYear : "2031", 
         preferredCcnCVV : "123",
         preferredProxyServer : '208.80.28.208:8080'
-
     },
+    bot2 : {           
+        preferredCategoryName : "jackets",
+        preferredTitle : "Eagle Hooded Work Jacket",
+        preferredColor : "Black", 
+        preferredSize : "Medium", 
+        preferredQuantity : "4", 
+        preferredBillingName : "Test One", 
+        preferredOrder_email : "testone@testone.com", 
+        preferredOrder_number : "123 456 789 0", 
+        preferredOrder_billing_address : "2181 PO BOX", 
+        preferredOrder_billing_city : "ALABASTER", 
+        preferredOrder_billing_zip : "35007-2024", 
+        preferredOrder_billing_state : "AL", 
+        preferredCreditCardNumber : "12312321321312321312", 
+        preferredCcnMonth : "10", 
+        preferredCcnYear : "2031", 
+        preferredCcnCVV : "123",
+        preferredProxyServer : '216.21.18.193:80'
+    }    ,
     bot3 : {
         preferredCategoryName : "skate",
         preferredTitle : "Supreme®/Spitfire® Classic Wheels(Set of 4)",
+        preferredColor : "Black", 
+        preferredSize : "XLarge", 
+        preferredQuantity : "4", 
+        preferredBillingName : "Test Three", 
+        preferredOrder_email : "testthree@testthree.com", 
+        preferredOrder_number : "123 456 789 0", 
+        preferredOrder_billing_address : "2181 PO BOX", 
+        preferredOrder_billing_city : "ALABASTER", 
+        preferredOrder_billing_zip : "35007-2024", 
+        preferredOrder_billing_state : "AL", 
+        preferredCreditCardNumber : "12312321321312321312", 
+        preferredCcnMonth : "10", 
+        preferredCcnYear : "2031", 
+        preferredCcnCVV : "123",
+        preferredProxyServer : '216.21.18.194:80'
+    },
+    bot4 : {
+        preferredCategoryName : "jackets",
+        preferredTitle : "Barn Coat",
         preferredColor : "Black", 
         preferredSize : "XLarge", 
         preferredQuantity : "4", 
@@ -74,5 +92,6 @@ const userBot = {
 
 }
 
-
-supreme.checkout(userBot);
+for(var counter in userBotData){        
+    supreme.checkout(userBotData[counter]);
+}
--- shop/supreme.js
+++ shop/supreme.js
@@ -3,98 +3,60 @@ const useProxy = require('puppeteer-page-proxy');
 
 // Global Variables
 const rand_url = "https://www.supremenewyork.com/shop/all";
-// const rand_url = "https://whatismyipaddress.com/";
-let preferredCategoryName = "";
-let preferredTitle = "";
-let preferredColor = "";
-let preferredSize = "";
-let preferredQuantity = "";
-let preferredBillingName = "";
-let preferredOrder_email = "";
-let preferredOrder_number = "";
-let preferredOrder_billing_address = "";
-let preferredOrder_billing_city = "";
-let preferredOrder_billing_zip = "";
-let preferredOrder_billing_state = "";
-let preferredCreditCardNumber = "";
-let preferredCcnMonth = "";
-let preferredCcnYear = "";
-let preferredCcnCVV = "";
-let preferredProxyServer = "";
 
+// The function being called on the bot.js to trigger all functions
+async function checkout(userBotData){ 
+    console.log("Checkout Called For : " + userBotData["preferredTitle"]);         
+    const page = await initBrowser(userBotData);   
+}
+module.exports = checkout;
+module.exports.checkout = checkout;
 
-async function initBrowser(preferredProxyServer){
+async function initBrowser(userBotData){
+    let preferredProxyServer = userBotData["preferredProxyServer"];
     const proxyServer = '--proxy-server=' + preferredProxyServer;
 
     const browser = await puppeteer.launch({
         headless: false,
-        defaultViewport: null,
         ignoreHTTPSErrors: true
         // args: [ proxyServer ]
     });
-    const page = await browser.newPage();    
+    const page = await browser.newPage();   
+
     // Set page viewport
-    await page.setViewport({ width: 1920, height: 1080, deviceScaleFactor: 1, });
+    // await page.setViewport({ width: 960, height: 900, deviceScaleFactor: 1, });
+    await page.setViewport({ width: 480, height: 450, deviceScaleFactor: 1, });
     // Close unused page
     const pages = await browser.pages();
     if (pages.length > 1) {
         await pages[0].close();
-    }
-    // Got to url
-    await page.setDefaultNavigationTimeout(0);
-    await page.goto(rand_url);
-    return page;
-}
-
-async function checkout(userBot){
-    for(var counter in userBot){
-        preferredCategoryName = userBot[counter]["preferredCategoryName"];
-        preferredTitle = userBot[counter]["preferredTitle"];
-        preferredColor = userBot[counter]["preferredColor"];
-        preferredSize = userBot[counter]["preferredSize"];
-        preferredQuantity = userBot[counter]["preferredQuantity"];
-        preferredBillingName = userBot[counter]["preferredBillingName"];
-        preferredOrder_email = userBot[counter]["preferredOrder_email"];
-        preferredOrder_number = userBot[counter]["preferredOrder_number"];
-        preferredOrder_billing_address = userBot[counter]["preferredOrder_billing_address"];
-        preferredOrder_billing_city = userBot[counter]["preferredOrder_billing_city"];
-        preferredOrder_billing_zip = userBot[counter]["preferredOrder_billing_zip"];
-        preferredOrder_billing_state = userBot[counter]["preferredOrder_billing_state"];
-        preferredCreditCardNumber = userBot[counter]["preferredCreditCardNumber"];
-        preferredCcnMonth = userBot[counter]["preferredCcnMonth"];
-        preferredCcnYear = userBot[counter]["preferredCcnYear"];
-        preferredCcnCVV = userBot[counter]["preferredCcnCVV"];
-        preferredProxyServer = userBot[counter]["preferredProxyServer"];
-
-        const pagecounter = await initBrowser(preferredProxyServer);
-        await removeSoldOutProduct(pagecounter);
-        await selectAvailProdByCategory(pagecounter);
-        selectProdNameCat(pagecounter, userBot[counter]);
-    }
+    }   
     
+    // Remove Page Timeout
+    await page.setDefaultNavigationTimeout(0);
+    // Got to url
+    await page.goto(rand_url);
+    selectAvailProdByCategory(page, userBotData);
+    // return page;    
 }
-
-module.exports = checkout;
-module.exports.checkout = checkout;
 
 // Remove sold out items
+// await removeSoldOutProduct(page); useless
 async function removeSoldOutProduct(page){
-
     let itemSoldOut = ".sold_out_tag";
-
     await page.evaluate((itemSoldOut) => {
         var elements = document.querySelectorAll(itemSoldOut);
-        for(var i=0; i< elements.length; i++){
+        for(var i=0; i< elements.length; i++){ 
             elements[i].parentNode.parentNode.parentNode.removeChild(elements[i].parentNode.parentNode);
         }
     }, itemSoldOut);    
 }
 
 // Select Available Product By Category
-async function selectAvailProdByCategory(page){
-
+async function selectAvailProdByCategory(page, userBotData){
+    
+    let preferredCategoryName = userBotData["preferredCategoryName"];
     let itemAvailable = ".inner-article";
-
     await page.evaluate((itemAvailable, preferredCategoryName) => {        
         var elements = document.querySelectorAll(itemAvailable); 
         for(var i=0; i< elements.length; i++){
@@ -104,105 +66,122 @@ async function selectAvailProdByCategory(page){
             }            
         }
     }, itemAvailable, preferredCategoryName);
+    await page.waitForSelector('h1[itemprop="name"]');
+    selectProdNameCat(page, userBotData);
 }
 
 // Select Product by Product Name in a Category
-async function selectProdNameCat(page, data){
-    console.log(data);
-    preferredTitle1 = data["preferredTitle"];
-    await page.waitForSelector('h1[itemprop="name"]', {visible: true})
+async function selectProdNameCat(page, userBotData){
+    
+    let preferredTitle = userBotData["preferredTitle"];
     let titleElement = await page.$('h1[itemprop="name"]');
     let itemTitle = await page.evaluate(el => el.textContent, titleElement);
-    await page.waitForTimeout(1000);
-
-    console.log(preferredTitle1 + " = " + itemTitle);
-
-    if( preferredTitle1 == itemTitle ){
-        addToCart(page, data);
+    if( preferredTitle == itemTitle ){
+        console.log("TRUE : " + preferredTitle + " = " + itemTitle);
+        addToCart(page, userBotData);
     }else{
+        console.log("FALSE : " + preferredTitle + " = " + itemTitle);
         await page.$eval("a[class='next']", elem => elem.click()); // color picker
         await page.waitForTimeout(1500);
-        selectProdNameCat(page);
+        selectProdNameCat(page, userBotData);
     }
-
+    console.log("**********************");
 }
 
 // Bot on Add To Cart Page
-async function addToCart(page, data){
-    // If color option exist
-    prefferedColor = data["preferredColor"];
-    const colorElement = await page.evaluate((preferredColor) => {
-        const element = document.querySelector("a[data-style-name='"+preferredColor+"']");        
-        return element;
-    }, preferredColor);
-    if(colorElement !== null){
-        await page.$eval("a[data-style-name='"+preferredColor+"']", elem => elem.click()); // color picker
-    }
+async function addToCart(page, userBotData){
 
-    // If sizes Exist
-    preferredSize = data["preferredSize"];
-    const sizeElement = await page.evaluate(() => {
-        const element = document.querySelector('select#size');        
-        return element;
+    let preferredTitle = userBotData["preferredTitle"];
+    let preferredColor = userBotData["preferredColor"];
+    let preferredSize = userBotData["preferredSize"];
+    let preferredQuantity = userBotData["preferredQuantity"];
+
+    // Check if class sold-out exist
+    const soldOut = await page.evaluate((soldOut) => {
+        const element = document.querySelector(".sold-out");
+        if(!element){
+            return false
+        }else{
+            return true;
+        }
     });
-    if(sizeElement !== null){
-        let $elemHandler = await page.$('select#size');
-        let properties = await $elemHandler.getProperties();
-        for (const property of properties.values()) {
-            const element = property.asElement();
-            if (element){
-                let hText = await element.getProperty("text");
-                let text = await hText.jsonValue();
-                if(text===preferredSize){
-                    let hValue = await element.getProperty("value");
-                    let value = await hValue.jsonValue();
-                    await page.select("select#size",value);
-                }
+    // If sold-out exist close browser return status
+    if(soldOut === true){
+        page.close();
+        console.log(preferredTitle + " is already Sold Out! Stop Process!");
+    }else if(soldOut === false){
+        console.log(preferredTitle + " is Available for Checkout! Continue Process!");
+        // If sold-out does not exist continue checkout        
+        // If color option exist        
+        const colorElement = await page.evaluate((preferredColor) => {
+            const element = document.querySelector("a[data-style-name='"+preferredColor+"']");        
+            return element;
+        }, preferredColor);
+        if(colorElement !== null){
+            await page.$eval("a[data-style-name='"+preferredColor+"']", elem => elem.click()); // color picker
+        }
+
+        // If sizes Exist
+        const sizeElement = await page.evaluate(() => {
+            const element = document.querySelector('select#size');        
+            return element;
+        });
+        if(sizeElement !== null){
+            var options = await page.$$eval("select#size option", all => all.map(a => a.textContent))
+            var option = options.includes(preferredSize);
+            console.log(options);
+            if(option === true){
+                const option = (await page.$x(
+                    '//*[@id = "size"]/option[text() = "'+preferredSize+'"]'
+                ))[0];
+                const value = await (await option.getProperty('value')).jsonValue();
+                await page.select('#size', value);
+            }else{
+                console.log("Option does not exist");
+            }            
+        }
+
+        // If Quantity Exist
+        const qtyElement = await page.evaluate(() => {
+            const element = document.querySelector('select#qty');        
+            return element;
+        });
+        if(qtyElement !== null){
+            await page.select("select#qty", preferredQuantity); // Quantity select
+        }
+
+        // If Add To Cart Button Exist
+        const addToCartElement = await page.evaluate(() => {
+            const element = document.querySelector("input[type='submit']");        
+            return element;
+        });
+        if(addToCartElement !== null){
+            await page.$eval("input[type='submit']", elem => elem.click()); // add to cart button
+            await page.waitForTimeout(1500);
+            await page.$eval("a[class='button checkout']", elem => elem.click()); // checkout button
+            try {
+                await page.waitForSelector('input#order_billing_name');
+                checkoutFormPage(page, userBotData); 
+            } catch (e) {
+                console.log('Element Does Not Exist');
             }
         }
-    }
-
-    // If Quantity Exist
-    preferredQuantity = data["preferredQuantity"];
-    const qtyElement = await page.evaluate(() => {
-        const element = document.querySelector('select#qty');        
-        return element;
-    });
-    if(qtyElement !== null){
-        await page.select("select#qty", preferredQuantity); // Quantity select
-    }
-
-    // If Add To Cart Button Exist
-    const addToCartElement = await page.evaluate(() => {
-        const element = document.querySelector("input[type='submit']");        
-        return element;
-    });
-    if(addToCartElement !== null){
-        await page.$eval("input[type='submit']", elem => elem.click()); // add to cart button
-        await page.waitForTimeout(1500);
-        await page.$eval("a[class='button checkout']", elem => elem.click()); // checkout button
-        await page.waitForTimeout(5000);
-        checkoutFormPage(page, data); // Proceed to filling out checkout form
-    }else{
-        page.close();
     }
 }
 
 // Bot on Delivery Page
-async function checkoutFormPage(page, data){
-    console.log(data);
-    preferredBillingName = data["preferredBillingName"];
-    preferredOrder_email = data["preferredOrder_email"];
-    preferredOrder_number = data["preferredOrder_number"];
-    preferredOrder_billing_address = data["preferredOrder_billing_address"];
-    preferredOrder_billing_zip = data["preferredOrder_billing_zip"];
-    preferredOrder_billing_city = data["preferredOrder_billing_city"];
-    preferredOrder_billing_state = data["preferredOrder_billing_state"];
-    preferredCreditCardNumber = data["preferredCreditCardNumber"];
-    preferredCcnMonth = data["preferredCcnMonth"];
-    preferredCcnYear = data["preferredCcnYear"];
-    preferredCcnCVV = data["preferredCcnCVV"];
-
+async function checkoutFormPage(page, userBotData){
+    let preferredBillingName = userBotData["preferredBillingName"];
+    let preferredOrder_email = userBotData["preferredOrder_email"];
+    let preferredOrder_number = userBotData["preferredOrder_number"];
+    let preferredOrder_billing_address = userBotData["preferredOrder_billing_address"];
+    let preferredOrder_billing_city = userBotData["preferredOrder_billing_city"];
+    let preferredOrder_billing_zip = userBotData["preferredOrder_billing_zip"];
+    let preferredOrder_billing_state = userBotData["preferredOrder_billing_state"];
+    let preferredCreditCardNumber = userBotData["preferredCreditCardNumber"];
+    let preferredCcnMonth = userBotData["preferredCcnMonth"];
+    let preferredCcnYear = userBotData["preferredCcnYear"];
+    let preferredCcnCVV = userBotData["preferredCcnCVV"];
 
     await page.type("input[id='order_billing_name']", preferredBillingName); // Write Full Name
     await page.waitForTimeout(1500);
